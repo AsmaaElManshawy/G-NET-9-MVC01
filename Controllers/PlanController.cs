41e3e9c6c29d1e828059b18e20517d8a96daf089
AsmaaElManshawy <sayedasmaa2001@gmail.com>
2026-05-22 22:42:51 +0300

    Plan Index and Details & Home Index & Layout

---
+++ Controllers/PlanController.cs
@@ -0,0 +1,34 @@
+﻿using GymSystem.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymSystem.Controllers
+{
+    public class PlanController : Controller
+    {
+        // Database connection
+        private readonly GymDbContet context;
+
+        public PlanController()
+        {
+            context = new GymDbContet();
+        }
+
+        // GET: /Plan/Index
+        public async Task<IActionResult> Index()
+        {
+            var plans = await context.Plans.ToListAsync();
+            return View(plans);
+        }
+
+        // GET: /Plan/Details/{id}
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await context.Plans.FindAsync(id);
+            if (plan == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(plan);
+        }
+    }
+}
--- FluentConfigurations/PlanConfiguration.cs
+++ FluentConfigurations/PlanConfiguration.cs
@@ -5,8 +5,29 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace GymSystem.FluentConfigurations
 {
-    public class PlanConfiguration : IEntityTypeConfiguration<Plans>
+    public class PlanConfiguration : IEntityTypeConfiguration<Plan>
     {
-       
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.Property(p => p.Name)
+                .HasColumnType("varchar")
+                .HasMaxLength(30);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(200);
+
+            builder.Property(p => p.Price)
+                .HasPrecision(10, 2);
+
+            builder.Property(p => p.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.ToTable(TB =>
+            {
+                TB.HasCheckConstraint("PlanDurationCheck", "DurationDays Between  1 and 365");
+            });
+
+        }
+
     }
 }
